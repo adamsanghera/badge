95f18e04cc517f8f5acbbff6d5b43582d9e8aeb8
Your Name <you@example.com>
2018-02-01 19:57:06 -0500

    moved a lot of badge functionality to category

--- badge.go
+++ badge.go
@@ -1,21 +1,33 @@
 package badge
 
 import (
-	"time"
-
-	"github.com/adamsanghera/auth"
-	"github.com/adamsanghera/category"
+	"crypto/rand"
+	"encoding/hex"
 )
 
 // Badge is an interface for services providing identity insurance.
 // Badges are a passport, certifying a recent check of citizenship.
 type Badge interface {
-	// Commands
-	Revoke() error
+	GetID() interface{}
+	Mint() error
+}
 
-	// Queries
-	BelongsTo() category.Category
-	Create() (uid interface{}, bid interface{}, badgeScanner auth.Authenticator, timeToLive time.Duration, err error)
-	Refresh() (uid interface{}, bid interface{}, badgeScanner auth.Authenticator, timeToLive time.Duration, err error)
-	Validate(uid interface{}, bid interface{}, badgeScanner auth.Authenticator) (bool, error)
+// Token implements Badge
+type Token struct {
+	tokenLength int
+	hash        string
+}
+
+func (t Token) GetID() interface{} {
+	return t.hash
+}
+
+func (t Token) Mint() error {
+	bitString := make([]byte, t.tokenLength)
+	_, err := rand.Read(bitString)
+	if err != nil {
+		return err
+	}
+	t.hash = hex.EncodeToString(bitString)
+	return nil
 }
